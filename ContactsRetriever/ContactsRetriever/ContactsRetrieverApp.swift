37f9dd13e447b46c4435b06f275c4cfb11a5d453
Florin Velesca <velesca.florin97@gmail.com>
2024-02-09 13:51:02 +0200

    initial commit

---
+++ ContactsRetriever/ContactsRetriever/ContactsRetrieverApp.swift
@@ -0,0 +1,16 @@
+//
+//  ContactsRetrieverApp.swift
+//  ContactsRetriever
+//
+//  Created by Florin Velesca on 09.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactsRetrieverApp: App {
+    var body: some Scene {
+        WindowGroup {
+        }
+    }
+}
